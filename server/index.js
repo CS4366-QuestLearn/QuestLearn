feebc35cf8d6ac6e3b02931e67ac850d9e5a6b68
Maureen Gloria <maureenmgloria@gmail.com>
2021-03-20 14:39:03 -0500

    change root message

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ var app = express()
 // var mongoose = require('mongoose')
 
 
-app.get('/', (req, res) => res.send('something something sometihing'));
+app.get('/', (req, res) => res.send('Hello world'));
 app.use(cors())
 app.use('/api', require('./example/foobar'))
 // app.use('/api', require('./utils/auth'))
