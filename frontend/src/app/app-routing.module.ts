61fbb280c863f99bf6c066e3042534c71a1df785
Kroren <tak4506@gmail.com>
2021-03-27 18:30:12 -0500

    Made the base style of student home

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -24,6 +24,7 @@ const routes: Routes = [
     ]
   },
   { path: 'login', component: LoginComponent },
+  { path: 'tyler', component: StudentHomeComponent },
   { path: '**', redirectTo: '' } // otherwise redirect to home
 ];
 
