53e9efca2cfc40cacd337fbd115e2dde65b15ac2
Maureen Gloria <maureenmgloria@gmail.com>
2021-03-31 21:58:21 -0500

    try fix async

--- server/utils/google.js
+++ server/utils/google.js
@@ -42,8 +42,10 @@ async function getClassroom(req, res) {
 }
 var subscription
 async function getClassrooms(req, res) {
+  console.log('geting classrooms')
   subscription = pubSubClient.subscription("my-topic-sub");
   if(req.query.user_type == "1") {
+    console.log('user is a teacher')
   classroom.courses.list(
     {
       teacherId: 'me'
