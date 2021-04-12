233b13fb6fb3d5ccb3eb78f0e56f3bb0ed9444e5
Maureen Gloria <maureenmgloria@gmail.com>
2021-04-12 04:51:42 -0500

    Fix quest-board formatting for quest elements

--- frontend/src/app/modules/quest-board/quest-board.component.ts
+++ frontend/src/app/modules/quest-board/quest-board.component.ts
@@ -10,11 +10,22 @@ export class QuestBoardComponent implements OnInit {
   public teamQuests = [
     { description: 'Donate to the Class Bank', progress: '300/500', due_date: 'Due March 20th', reward: 'Pizza Party' },
     { description: 'Participate in Lab', progress: 'Entire Class', due_date: 'Due March 15th', reward: '5 QC' },
+    { description: 'Participate in Lab', progress: 'Entire Class', due_date: 'Due March 15th', reward: '5 QC' },
+    { description: 'Participate in Lab', progress: 'Entire Class', due_date: 'Due March 15th', reward: '5 QC' },
+    { description: 'Participate in Lab', progress: 'Entire Class', due_date: 'Due March 15th', reward: '5 QC' },
+    { description: 'Participate in Lab', progress: 'Entire Class', due_date: 'Due March 15th', reward: '5 QC' },
+    { description: 'Participate in Lab', progress: 'Entire Class', due_date: 'Due March 15th', reward: '5 QC' },
   ];
   
   public yourQuests = [
     { description: 'Homework 1', progress: 'Turn In', due_date: 'Due March 11th', reward: '10 QC' },
     { description: 'Homework 2', progress: 'Turn In', due_date: 'Due March 16th', reward: '5 QC' },
+    { description: 'Reading Analysis', progress: 'Turn In', due_date: 'Due March 19th', reward: '8 QC' },
+    { description: 'Reading Analysis', progress: 'Turn In', due_date: 'Due March 19th', reward: '8 QC' },
+    { description: 'Reading Analysis', progress: 'Turn In', due_date: 'Due March 19th', reward: '8 QC' },
+    { description: 'Reading Analysis', progress: 'Turn In', due_date: 'Due March 19th', reward: '8 QC' },
+    { description: 'Reading Analysis', progress: 'Turn In', due_date: 'Due March 19th', reward: '8 QC' },
+    { description: 'Reading Analysis', progress: 'Turn In', due_date: 'Due March 19th', reward: '8 QC' },
     { description: 'Reading Analysis', progress: 'Turn In', due_date: 'Due March 19th', reward: '8 QC' },
   ];
 
